5f3c64e3d2209b6767c13ecf4aaf42d93fff0c5b
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-05-17 22:41:13 +0300

    Add shiny random walk

---
+++ examples/random_walk_shiny/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/divan/goabm/abm"
+	"github.com/divan/goabm/ui/shiny_grid"
+	"github.com/divan/goabm/worlds/grid2d"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	a := abm.New()
+	w, h := 100, 80
+	grid2D := grid.New(w, h)
+	a.SetWorld(grid2D)
+
+	cell := NewWalker(a, 50, 40)
+	a.AddAgent(cell)
+	grid2D.SetCell(cell.x, cell.y, cell)
+
+	a.LimitIterations(1000)
+
+	ch := make(chan [][]interface{})
+	a.SetReportFunc(func(a *abm.ABM) {
+		ch <- grid2D.Dump(func(a abm.Agent) bool { return a != nil })
+		time.Sleep(10 * time.Millisecond)
+	})
+
+	go func() {
+		a.StartSimulation()
+		close(ch)
+	}()
+
+	ui := shiny.New(w, h)
+	defer ui.Stop()
+	ui.AddGrid(ch)
+	ui.Loop()
+}
